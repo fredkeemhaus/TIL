1178586a6027d6914e9cc3100cb343323e375147
fredkeemhaus <fredkeemhaus@gmail.com>
2021-11-11 19:48:22 +0900

    init

---
+++ algorithm/section02/section02-2.py
@@ -0,0 +1,20 @@
+# Section02-2
+# 파이썬 기초 코딩
+# 몸풀기 코딩 실습
+
+# import this
+import sys
+
+# 파이썬 기본 인코딩 
+print((sys.stdin.encoding))
+print((sys.stdout.encoding))
+
+# 출력문
+print('My name is Goodboy!')
+
+# 변수 선언
+myName = 'GoodBoy'
+
+# 조건문
+if myName == 'GoodBoy':
+    print('Ok')
