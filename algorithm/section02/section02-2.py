f5e88f645e99c16174f50214f8c987fe2e0ac4dc
fredkeemhaus <fredkeemhaus@gmail.com>
2021-11-11 19:58:13 +0900

    section02-2

--- algorithm/section02/section02-2.py
+++ algorithm/section02/section02-2.py
@@ -18,3 +18,34 @@ myName = 'GoodBoy'
 # 조건문
 if myName == 'GoodBoy':
     print('Ok')
+
+# 반복문
+for i in range(1, 10):
+    for j in range(1, 10):
+        print('%d * %d ='%(i, j), i * j) # 구구단
+
+# 변수 선언 (한글)
+이름 = '좋은사람'
+print(이름)
+
+# 함수 선언 (한글)
+def 인사():
+    print('안녕하세요. 반갑습니다')
+
+인사()
+
+# 함수 선언 (영어)
+def greeting():
+    print('Hello!')
+
+greeting()
+
+# 클래스
+class Cookie:
+    pass
+
+# 객체 생성
+cookie = Cookie()
+
+print(id(cookie))
+print(dir(cookie))
