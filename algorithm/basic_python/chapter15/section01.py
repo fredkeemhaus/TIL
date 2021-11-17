9a4db028775a7aa51832cde2d2351e39728c5907
fredkeemhaus <fredkeemhaus@gmail.com>
2021-11-17 22:59:49 +0900

    TIL merge sort example

--- algorithm/basic_python/chapter15/section01.py
+++ algorithm/basic_python/chapter15/section01.py
@@ -44,15 +44,52 @@ print(qsort([1,6,5,2,3,10]))
 # -- 2. 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
 # -- 3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다.
 
-# def merge(left, right):
-#     # if left[lp] >
 
-def split(data):
+import random
+data_list = random.sample(range(100), 10)
+
+# def split(data):
+#     medium = int(len(data) / 2)
+
+#     left = data[:medium]
+#     right = data[medium:]
+
+#     print(left, right)
+
+def mergesplit(data):
+    if len(data) <= 1:
+        return data
+
     medium = int(len(data) / 2)
 
-    left = data[:medium]
-    right = data[:medium]
+    left = mergesplit(data[:medium])
+    right = mergesplit(data[medium:])
 
-    print(left, right)
+    return merge(left, right)
 
-print(split([3, 4, 1, 3, 2]))
+def merge(left, right):
+    merged = []
+    left_point, right_point = 0, 0
+
+    # case1: left/right 아직 남아있을 때,
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    # case2: left만 남아있을 때,
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+
+    # case3: right만 남아있을 때,
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+
+    return merged
+
+print(mergesplit(data_list))
