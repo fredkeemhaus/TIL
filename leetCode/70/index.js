bf3ad21d3f24de9bdebec85cb6e76e9e0a2229b3
fredkeemhaus <fredkeemhaus@gmail.com>
2021-12-28 01:19:33 +0900

    leetcode 70 solved

---
+++ leetCode/70/index.js
@@ -0,0 +1,12 @@
+var climbStairs = function (n) {
+  if (n < 3) return n;
+  let dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+};
