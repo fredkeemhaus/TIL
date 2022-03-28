98538444d4176b6caa2f448695662896eede3d6f
fredkeemhaus <fredkeemhaus@gmail.com>
2022-03-28 18:49:40 +0900

    홀수

---
+++ inflearn/javascriptAlgorithm/007/index.js
@@ -0,0 +1,20 @@
+const solution = (arr) => {
+  let answer = [];
+  let sum = 0,
+    min = Number.MAX_SAFE_INTEGER;
+
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      sum += x;
+      if (x < min) min = x;
+    }
+  }
+
+  answer.push(sum);
+  answer.push(min);
+
+  return answer;
+};
+
+const arr = [12, 77, 38, 41, 53, 92, 85];
+solution(arr);
