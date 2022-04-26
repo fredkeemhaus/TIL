3c9cebc70c141dba78d95511dbeaf92cb0ae4564
fredkeemhaus <fredkeemhaus@gmail.com>
2022-04-26 19:37:47 +0900

    2-9 List

--- ReactOfficialDocument/src/App.js
+++ ReactOfficialDocument/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Event from "./components/2-7.Event/Event";
 import Condition from "./components/2-8.ConditionalRendering/Condition";
+import List from "./components/2-9.List/List";
 // import ClassComponent from "./components/2-5.State/ClassComponent";
 // import Functionalcomponent from "./components/2-5.State/FunctionalComponent";
 // import ClassComponent from "./components/2-6.LifeCycle/ClassComponent";
@@ -11,7 +12,8 @@ function App() {
       {/* <Functionalcomponent /> */}
       {/* <ClassComponent /> */}
       {/* <Event /> */}
-      <Condition />
+      {/* <Condition /> */}
+      <List />
     </div>
   );
 }
--- ReactOfficialDocument/src/components/2-9.List/List.jsx
+++ ReactOfficialDocument/src/components/2-9.List/List.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
+// map: 배열의 개별 요소를 순회
+// default key: 주지 않으면 react는 index를 쓴다. (warning 뜸)
+// 고유성: 형제 사이에서만 고유하면 된다.
+// key props: key는 props로 넘어가지 않는다.
+
 const List = () => {
-  return <div></div>;
+  //   const numbers = [1, 2, 3, 4, 5];
+  //   return (
+  //     <div>
+  //       <ul>
+  //         {numbers.map((item) => (
+  //           <li key={item.toString()}>{item}</li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   );
+  // };
+
+  const todos = [
+    { id: 1, text: "Drink Water" },
+    { id: 2, text: "Wash Car" },
+    { id: 3, text: "Listen Lecture" },
+    { id: 4, text: "Go to bed" },
+  ];
+
+  const Item = (props) => {
+    return <li>{props.text}</li>;
+  };
+
+  const todoList = todos.map((todo) => <Item key={todo.id} {...todo} />);
+
+  return <>{todoList}</>;
 };
 
 export default List;
