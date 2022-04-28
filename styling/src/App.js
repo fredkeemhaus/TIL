2e1f80aa367302d196841461ba78a3ba929a3ef0
fredkeemhaus <fredkeemhaus@gmail.com>
2022-04-28 19:20:42 +0900

    styled component

---
+++ styling/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Styledcomponentsexample from "./components/StyledComponentsExample/StyledComponentsExample";
+
+function App() {
+  return (
+    <div className="App">
+      <Styledcomponentsexample />
+    </div>
+  );
+}
+
+export default App;
---
+++ styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+
+// Create a Title component that'll render an <h1> tag with some styles
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
+
+// // Create a Wrapper component that'll render a <section> tag with some styles
+// const Wrapper = styled.section`
+//   padding: 4em;
+//   background: papayawhip;
+// `;
+
+// const Button = styled.button`
+//   /* Adapt the colors based on primary prop */
+//   background: ${(props) => (props.primary ? "palevioletred" : "white")};
+//   color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+//   font-size: 1em;
+//   margin: 1em;
+//   padding: 0.25em 1em;
+//   border: 2px solid palevioletred;
+//   border-radius: 3px;
+// `;
+
+// const TomatoButton = styled(Button)`
+//   border-color: tomato;
+//   color: tomato;
+// `;
+
+// const ReversedButton = (props) => (
+//   <Button {...props} children={props.children.split("").reverse()} />
+// );
+
+const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+  color: blue;
+
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+
+  & ~ & {
+    background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+  }
+
+  & + & {
+    background: lime; // <Thing> next to <Thing>
+  }
+
+  &.something {
+    background: orange; // <Thing> tagged with an additional CSS class ".something"
+  }
+
+  .something-else & {
+    border: 1px solid; // <Thing> inside another element labeled ".something-else"
+  }
+`;
+
+const Styledcomponentsexample = () => {
+  return (
+    <>
+      <Thing>Hello world!</Thing>
+      <Thing>How ya doing?</Thing>
+      <Thing className="something">The sun is shining...</Thing>
+      <div>Pretty nice day today.</div>
+      <Thing>Don't you think?</Thing>
+      <div className="something-else">
+        <Thing>Splendid.</Thing>
+      </div>
+    </>
+  );
+};
+
+export default Styledcomponentsexample;
